5fde34cfd4433aef05bb16c9b30e6dc45e8eed74
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2021-05-15 20:28:50 +0200

    Update

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,14 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'jest-coverage',
   collectCoverageFrom: ['src/*.ts', 'src/**/*.ts'],
-  coveragePathIgnorePatterns: ['index.ts', '__quicktype-contract-*.ts', '__testdata__/'],
+  coveragePathIgnorePatterns: ['/__testdata__/', '/build/'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
   moduleFileExtensions: ['js', 'ts'],
+  modulePathIgnorePatterns: ['<rootDir>/build'],
   testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
   testTimeout: 30000
 };
